81d0d7e863aa8f96441439243a12858dde8cde10
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-15 19:12:59 -0400

    docs: documentation for the verification codes responses

--- src/controller/responses/verificationCodeRes.js
+++ src/controller/responses/verificationCodeRes.js
@@ -1,10 +1,22 @@
 import VerificationCode from '../../models/VerificationCode.js';
 
+/**
+ * Sends all verification codes as a JSON response.
+ * 
+ * @param {*} request - The request object.
+ * @param {*} response - The response object.
+ */
 export const sendAllCodes = async (request, response) => {
 	const codes = await VerificationCode.find();
 	response.json(codes);
 };
 
+/**
+ * Sends a verification code by its ID as a JSON response.
+ * 
+ * @param {*} request - The request object.
+ * @param {*} response - The response object.
+ */
 export const sendCodeById = (request, response) => {
 	const code = findById(request.params.id);
 	response.json(code);
