ba7dafb83962ca118d21618f9fbe0c6cbc8397ea
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-15 19:13:40 -0400

    docs: documentation for the incidents responses

--- src/controller/responses/incidentRes.js
+++ src/controller/responses/incidentRes.js
@@ -1,11 +1,26 @@
 import Incident from '../../models/Incident.js';
 import { deleteDeathIncidents } from '../requests/incidentReq.js';
 
+/**
+ * Sends an incident by its ID as a JSON response.
+ * 
+ * @param {*} request - The request object.
+ * @param {*} response - The response object.
+ */
 export const sendIncidentById = async (request, response) => {
 	const incident = Incident.findById(request.params.id);
 	response.json(incident);
 };
 
+/**
+ * Sends a list of incidents as a JSON response.
+ * 
+ * If longitude, latitude, and radius are provided in the query parameters,
+ * it retrieves nearby incidents; otherwise, it retrieves all incidents.
+ * 
+ * @param {*} request - The request object.
+ * @param {*} response - The response object.
+ */
 export const sendIncidents = async (request, response) => {
 	const { longitude, latitude, radius } = request.query;
 	await deleteDeathIncidents();
@@ -18,11 +33,24 @@ export const sendIncidents = async (request, response) => {
 	response.json(incidents);
 };
 
+/**
+ * Retrieves all incidents.
+ * 
+ * @returns {Array} - The array of incidents.
+ */
 const getAllIncidents = async () => {
 	const incidents = await Incident.find();
 	return incidents;
 };
 
+/**
+ * Retrieves nearby incidents based on the provided longitude, latitude, and radius.
+ * 
+ * @param {number} longitude - The longitude coordinate.
+ * @param {number} latitude - The latitude coordinate.
+ * @param {number} radius - The radius in which to search for incidents.
+ * @returns {Array} - The array of nearby incidents.
+ */
 const getNearIncidents = async (longitude, latitude, radius) => {
 	try {
 		const referenceCoordinate = [parseFloat(longitude), parseFloat(latitude)];
