febb130cb77014543bae22323e894d5101b992e8
VillalobosGarciaGaby <gabriela.garcia.13355797@usalesiana.edu.bo>
2023-06-21 15:00:09 -0400

    task: delete comments in routes

--- src/routes/sendEmail.routes.js
+++ src/routes/sendEmail.routes.js
@@ -3,7 +3,6 @@ import { sendEmail } from '../controller/requests/sendEmailReq.js';
 
 const sendEmailRoute = Router();
 
-// Define a route for sending emails
 sendEmailRoute.post('/send-email', sendEmail);
 
 export default sendEmailRoute;
